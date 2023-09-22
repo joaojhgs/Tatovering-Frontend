87b3ce57c0ef10c17985630c30a2ef1525ec9857
diogorodriguees <diogorodrigueslife@gmail.com>
2023-09-21 21:26:25 -0300

    style: form da criação sem conexão com a API

--- src/app/[locale]/tatuagens/page.tsx
+++ src/app/[locale]/tatuagens/page.tsx
@@ -9,15 +9,15 @@ export default function Page() {
   const t = useTranslations('site');
 
   const [form] = useForm();
-  const onFinish = (values: any) => {
-    console.log('Received values of form: ', values);
-    axios.post(`${process.env.NEXT_PUBLIC_API_URL}/tatuagens`, {
-      desenho: values.desenho,
-      estilo: values.estilo,
-      cor: values.cor,
-      tamanho: values.tamanho,
-      preco: values.preco,
-    });
+  const registarTatuagem = (values: any) => {
+    console.log('Received values of form: ', values.desenho);
+    // axios.post(`${process.env.NEXT_PUBLIC_API_URL}/tatuagens`, {
+    //   desenho: values.desenho,
+    //   estilo: values.estilo,
+    //   cor: values.cor,
+    //   tamanho: values.tamanho,
+    //   preco: values.preco,
+    // });
   };
 
   return (
@@ -28,51 +28,46 @@ export default function Page() {
           'url(https://images.unsplash.com/photo-1479767574301-a01c78234a0c?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2070&q=80)',
       }}
     >
-      <div className="h-3/4 w-1/3 max-w-3xl items-center rounded-xl bg-black p-16 text-center">
-        <h2 className="mb-8 text-3xl font-bold">Resgistrar Tatuagem</h2>
+      <div className="h-4/5 w-1/4 max-w-3xl items-center rounded-xl bg-black p-12 text-center">
+        <h2 className="mb-10  text-3xl font-bold">Resgistrar Tatuagem</h2>
         <Form
           form={form}
           name="tatuagem"
           className="login-form  space-y-6"
           initialValues={{ remember: true }}
-          onFinish={onFinish}
+          onFinish={registarTatuagem}
         >
-          <Form.Item>
+          <Form.Item name="desenho">
             <Input
               type="text"
-              name="desenho"
               className="h-12 w-full border-b border-gray-400 py-2 text-base focus:border-indigo-500 focus:outline-none"
-              placeholder="Desenho"
+              placeholder="URL da Imagem"
             />
           </Form.Item>
-          <Form.Item>
+          <Form.Item name="estilo">
             <Input
               type="text"
-              name="estilo"
               className="h-12 w-full border-b border-gray-400 py-2 text-base focus:border-indigo-500 focus:outline-none"
               placeholder="Estilo"
             />
           </Form.Item>
-          <Form.Item>
+          <Form.Item name="cor">
             <Input
               type="text"
-              name="cor"
               className="h-12 w-full border-b border-gray-400 py-2 text-base focus:border-indigo-500 focus:outline-none"
               placeholder="Cor"
             />
           </Form.Item>
-          <Form.Item className="bg-green">
+          <Form.Item name="preco">
             <Input
               type="number"
-              name="preco"
               className="h-12 w-full border-b border-gray-400 py-2 text-base focus:border-indigo-500 focus:outline-none"
               placeholder="Preço"
             />
           </Form.Item>
-          <Form.Item>
+          <Form.Item name="tamanho">
             <Input
               type="number"
-              name="tamanho"
               className="h-12 w-full border-b border-gray-400 py-2 text-base focus:border-indigo-500 focus:outline-none"
               placeholder="Tamanho"
             />
@@ -81,9 +76,9 @@ export default function Page() {
             type="primary"
             htmlType="submit"
             size="large"
-            className="focus:shadow-outline flex h-12 w-full cursor-pointer justify-center rounded-sm py-2 font-semibold  tracking-wide  text-gray-100 shadow-lg transition duration-300 ease-in focus:outline-none"
+            className="focus:shadow-outline flex h-16 w-full cursor-pointer justify-center rounded-sm  font-semibold  tracking-wide  text-gray-100 shadow-lg transition duration-300 ease-in focus:outline-none"
           >
-            Registrar
+            Concluir
           </Button>
         </Form>
       </div>
